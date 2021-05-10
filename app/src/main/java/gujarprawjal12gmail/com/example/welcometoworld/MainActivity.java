b3409a47c8ee32c5377da8aa2a5beae2abb8a381
gujarprajwal12 <gujarprajwal12@gmail.com>
2021-05-10 14:57:17 +0530

    improve the GUI application

---
+++ app/src/main/java/gujarprawjal12gmail/com/example/welcometoworld/MainActivity.java
@@ -0,0 +1,17 @@
+package gujarprawjal12gmail.com.example.welcometoworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // it gaurent to exctued .
+        // rule  in andriod it madnatory to call base class method again
+        setContentView(R.layout.activity_main);
+
+    }
+}
